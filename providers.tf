64f1ecff178104894d56084c49a5c55ca868173a
Tiger <twatson@gitlab.com>
2022-04-14 16:44:17 +1000

    Use Helm to install the GitLab Agent

--- providers.tf
+++ providers.tf
@@ -2,8 +2,10 @@ provider "google" {
   project = var.gcp_project
 }
 
-provider "kubernetes" {
-  host                   = "https://${google_container_cluster.primary.endpoint}"
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
-  token                  = data.google_client_config.current.access_token
+provider "helm" {
+  kubernetes {
+    host                   = "https://${google_container_cluster.primary.endpoint}"
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+    token                  = data.google_client_config.current.access_token
+  }
 }
