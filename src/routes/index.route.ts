fcacfc6198c6bbf98a6dcf1fadba0f9dacafeaed
Dey Williams Gordillo <deygordillos@gmail.com>
2023-09-22 01:19:47 -0300

    Index Route Companies

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import authRoutes from './auth.route'
 import usersRoutes from './users.route'
 import groupsRoutes from './groups.route'
+import companiesRoutes from './companies.route'
 
 const router = Router();
 
@@ -9,5 +10,6 @@ router.use('/v1/', authRoutes);
 router.use('/v1/auth', authRoutes);
 router.use('/v1/users', usersRoutes);
 router.use('/v1/groups', groupsRoutes);
+router.use('/v1/companies', companiesRoutes);
 
 export default router
