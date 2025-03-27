41ad214f603b85ed6caea2bf6421af6f54eef089
Dey Williams Gordillo <deygordillos@gmail.com>
2025-03-27 00:23:27 -0300

    Se crea entidad de clientes

---
+++ src/entity/customers.entity.ts
@@ -0,0 +1,73 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    ManyToOne,
+    CreateDateColumn,
+    UpdateDateColumn,
+    Index,
+    JoinColumn
+} from "typeorm";
+import { Companies } from "./companies.entity";
+
+@Index("company_code_idfiscal", ["company_id", "cust_code", "cust_id_fiscal"])
+@Index("cust_description", ["cust_description"])
+@Index("cust_status", ["cust_status"])
+
+@Entity("Customers")
+export class Customers {
+    @PrimaryGeneratedColumn({ unsigned: true })
+    cust_id: number;
+
+    @ManyToOne(() => Companies, (company) => company.customers, { 
+        onDelete: "CASCADE", 
+        onUpdate: "CASCADE" 
+    })
+    @JoinColumn({ name: 'company_id' })
+    company_id: Companies;
+
+    @Column({ type: "varchar", length: 30, comment: "código de la empresa" })
+    cust_code: string;
+
+    @Column({ type: "varchar", length: 30, comment: "id fiscal de la empresa" })
+    cust_id_fiscal: string;
+
+    @Column({ type: "tinyint", width: 1, default: 1, comment: "1 activo, 0 inactivo" })
+    cust_status: number;
+
+    @Column({ type: "varchar", length: 30, comment: "nombre o descripción de la empresa" })
+    cust_description: string;
+
+    @Column({ type: "varchar", length: 200, nullable: true, comment: "dirección de la empresa" })
+    cust_address?: string;
+
+    @Column({ type: "varchar", length: 100, nullable: true, comment: "complemento de la dirección de la empresa" })
+    cust_address_complement?: string;
+
+    @Column({ type: "varchar", length: 100, nullable: true, comment: "ciudad dirección de la empresa" })
+    cust_address_city?: string;
+
+    @Column({ type: "varchar", length: 100, nullable: true, comment: "estado de la dirección de la empresa" })
+    cust_address_state?: string;
+
+    @Column({ type: "tinyint", width: 1, default: 0, comment: "exento de impuestos 1 si 0 no" })
+    cust_exempt: number;
+
+    @Column({ type: "varchar", length: 200, nullable: true, comment: "correo de la empresa" })
+    cust_email?: string;
+
+    @Column({ type: "varchar", length: 20, nullable: true, comment: "telefono contacto 1 de la empresa" })
+    cust_telephone1?: string;
+
+    @Column({ type: "varchar", length: 20, nullable: true, comment: "telefono contacto 2 de la empresa" })
+    cust_telephone2?: string;
+
+    @Column({ type: "varchar", length: 20, nullable: true, comment: "celular contacto de la empresa" })
+    cust_cellphone?: string;
+
+    @CreateDateColumn({ type: "datetime", default: () => "CURRENT_TIMESTAMP" })
+    created_at: Date;
+
+    @UpdateDateColumn({ type: "datetime", default: () => "CURRENT_TIMESTAMP", onUpdate: "CURRENT_TIMESTAMP" })
+    updated_at: Date;
+}
---
+++ src/migration/1743044472599-customers.ts
@@ -0,0 +1,167 @@
+import { MigrationInterface, QueryRunner, Table, TableForeignKey, TableIndex } from "typeorm";
+
+export class Customers_1743044472599 implements MigrationInterface {
+
+    table_name = 'Customers';
+            
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.createTable(
+            new Table({
+                name: this.table_name,
+                columns: [
+                    {
+                        name: "cust_id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        unsigned: true,
+                        generationStrategy: "increment"
+                    },
+                    {
+                        name: "company_id",
+                        type: "int",
+                        unsigned: true
+                    },
+                    {
+                        name: "cust_code",
+                        type: "varchar",
+                        length: "30",
+                        comment: "código de la empresa"
+                    },
+                    {
+                        name: "cust_id_fiscal",
+                        type: "varchar",
+                        length: "30",
+                        comment: "id fiscal de la empresa"
+                    },
+                    {
+                        name: "cust_status",
+                        type: "tinyint",
+                        default: 1,
+                        width: 1,
+                        comment: "1 activo, 0 inactivo"
+                    },
+                    {
+                        name: "cust_description",
+                        type: "varchar",
+                        length: "30",
+                        comment: "nombre o descripción de la empresa"
+                    },
+                    {
+                        name: "cust_address",
+                        type: "varchar",
+                        length: "200",
+                        isNullable: true,
+                        comment: "dirección de la empresa"
+                    },
+                    {
+                        name: "cust_address_complement",
+                        type: "varchar",
+                        length: "100",
+                        isNullable: true,
+                        comment: "complemento de la dirección de la empresa"
+                    },
+                    {
+                        name: "cust_address_city",
+                        type: "varchar",
+                        length: "100",
+                        isNullable: true,
+                        comment: "ciudad dirección de la empresa"
+                    },
+                    {
+                        name: "cust_address_state",
+                        type: "varchar",
+                        length: "100",
+                        isNullable: true,
+                        comment: "estado de la dirección de la empresa"
+                    },
+                    {
+                        name: "cust_exempt",
+                        type: "tinyint",
+                        default: 0,
+                        width: 1,
+                        comment: "exento de impuestos 1 si 0 no"
+                    },
+                    {
+                        name: "cust_email",
+                        type: "varchar",
+                        length: "200",
+                        isNullable: true,
+                        comment: "correo de la empresa"
+                    },
+                    {
+                        name: "cust_telephone1",
+                        type: "varchar",
+                        length: "20",
+                        isNullable: true,
+                        comment: "telefono contacto 1 de la empresa"
+                    },
+                    {
+                        name: "cust_telephone2",
+                        type: "varchar",
+                        length: "20",
+                        isNullable: true,
+                        comment: "telefono contacto 2 de la empresa"
+                    },
+                    {
+                        name: "cust_cellphone",
+                        type: "varchar",
+                        length: "20",
+                        isNullable: true,
+                        comment: "celular contacto de la empresa"
+                    },
+                    {
+                        name: "created_at",
+                        type: "datetime",
+                        default: "CURRENT_TIMESTAMP",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "datetime",
+                        default: "CURRENT_TIMESTAMP",
+                    },
+                ]
+            }),
+            true,
+        );
+
+        await queryRunner.createIndex(this.table_name, new TableIndex({
+            name: 'company_code_idfiscal',
+            columnNames: ['company_id', 'cust_code', 'cust_id_fiscal']
+        }))
+
+        await queryRunner.createIndex(this.table_name, new TableIndex({
+            name: 'cust_description',
+            columnNames: ['cust_description']
+        }))
+
+        await queryRunner.createIndex(this.table_name, new TableIndex({
+            name: 'cust_status',
+            columnNames: ['cust_status']
+        }))
+
+        await queryRunner.createForeignKey(
+            this.table_name,
+            new TableForeignKey({
+                columnNames: ["company_id"],
+                referencedColumnNames: ["company_id"],
+                referencedTableName: "Companies",
+                onUpdate: "CASCADE",
+                onDelete: "CASCADE",
+            }),
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        const table = await queryRunner.getTable(this.table_name);
+        const foreignKeyCompany = table.foreignKeys.find(fk => fk.columnNames.indexOf("company_id") !== -1);
+        await queryRunner.dropForeignKey(this.table_name, foreignKeyCompany);
+
+        await queryRunner.dropIndex(this.table_name, 'cust_status');
+        await queryRunner.dropIndex(this.table_name, 'cust_description');
+        await queryRunner.dropIndex(this.table_name, 'company_code_idfiscal');
+        await queryRunner.dropTable(this.table_name);
+    }
+
+}
